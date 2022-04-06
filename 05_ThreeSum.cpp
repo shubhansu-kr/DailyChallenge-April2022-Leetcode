dbce3be9babe7e88ccfac1788b7bb8ff9407664b
shubhansu-kr <shubhansu2021@gmail.com>
2022-04-06 20:09:10 +0530

    Time Limit Exceeded

--- 05_ThreeSum.cpp
+++ 05_ThreeSum.cpp
@@ -14,11 +14,49 @@ class Solution
 public:
     int threeSumMulti(vector<int> &arr, int target)
     {
-        
-        // Prone to TLE 
-        
-        //  Time limit exceeded as expected 
-        
+        // Same solution with conditions - Still the TLE probablity persists
+        // If the arr will be sorted we can add more conditions
+        sort(arr.begin(), arr.end());
+        int count = 0;
+        for (int i = 0; i < arr.size(); ++i)
+        {
+            if (arr[i] > target)
+            {
+                break;
+            }
+            for (int j = i + 1; j < arr.size(); ++j)
+            {
+                if (arr[i] + arr[j] > target)
+                {
+                    break;
+                }
+                for (int k = j + 1; k < arr.size(); ++k)
+                {
+                    if (arr[i] + arr[j] + arr[k] > target)
+                    {
+                        break;
+                    }
+                    if (arr[i] + arr[j] + arr[k] == target)
+                    {
+                        count++;
+                    }
+                }
+            }
+        }
+        return count;
+    }
+};
+
+class Solution
+{
+public:
+    int threeSumMulti(vector<int> &arr, int target)
+    {
+
+        // Prone to TLE
+
+        //  Time limit exceeded as expected
+
         int count = 0;
         for (int i = 0; i < arr.size(); i++)
         {
@@ -33,7 +71,7 @@ public:
                 }
             }
         }
-        return count ;
+        return count;
     }
 };
 
