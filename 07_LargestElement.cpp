7579427b47965d9ad7d13608f5ce66c09e317f23
shubhansu-kr <shubhansu2021@gmail.com>
2022-04-09 00:19:51 +0530

    Run time error

--- 07_LargestElement.cpp
+++ 07_LargestElement.cpp
@@ -18,6 +18,59 @@ class KthLargest
 {
 public:
     vector<int> v;
+    KthLargest(int k, vector<int> &nums)
+    {
+        if (nums.size())
+        {
+            sort(nums.begin(), nums.end());
+            for (int i = nums.size() - k; i < nums.size(); i++)
+            {
+                v.push_back(nums[i]);
+            }
+        }
+        else
+        {
+            v.push_back(-10001);
+        }
+    }
+
+    int add(int val)
+    {
+        if (v[0] >= val)
+        {
+            return v[0];
+        }
+        else
+        {
+            if (val >= v.back())
+            {
+                v.insert(v.begin() + v.size(), val);
+            }
+            else
+            {
+                for (int i = 0; i < v.size(); i++)
+                {
+                    if (val <= v[i])
+                    {
+                        v.insert(v.begin() + i, val);
+                        break;
+                    }
+                }
+            }
+            v.erase(v.begin());
+            return v[0];
+        }
+    }
+};
+
+class KthLargest
+{
+    // This solution is intitutive - Without much thinking
+    // Why would you consider storing all the val, when you literally
+    // need only the last k element
+
+public:
+    vector<int> v;
     int index;
     KthLargest(int k, vector<int> &nums)
     {
