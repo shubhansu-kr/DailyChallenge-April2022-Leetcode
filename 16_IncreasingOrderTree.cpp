0fd8904cbadabc2a65582e05409b534fea34477a
shubhansu-kr <shubhansu2021@gmail.com>
2022-04-17 15:17:02 +0530

    Unsolved

---
+++ 16_IncreasingOrderTree.cpp
@@ -0,0 +1,34 @@
+// https://leetcode.com/problems/increasing-order-search-tree/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+public:
+    TreeNode *increasingBST(TreeNode *root, TreeNode *tail = NULL)
+    {
+        if (!root)
+            return tail;
+        TreeNode *res = increasingBST(root->left, root);
+        root->left = NULL;
+        root->right = increasingBST(root->right, tail);
+        return res;
+    }
+};
+
+int main()
+{
+
+    return 0;
+}
