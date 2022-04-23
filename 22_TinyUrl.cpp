9a01c1c9c4f28c6d0da82d43f08a5aedef95eac0
shubhansu-kr <shubhansu2021@gmail.com>
2022-04-23 18:43:05 +0530

    Encode and decode URL

---
+++ 22_TinyUrl.cpp
@@ -0,0 +1,23 @@
+// https://leetcode.com/problems/encode-and-decode-tinyurl/submissions/
+
+#include <bits/stdc++.h>
+using namespace std ;
+
+class Solution {
+public:
+
+    // Encodes a URL to a shortened URL.
+    string encode(string longUrl) {
+        return longUrl ;
+    }
+
+    // Decodes a shortened URL to its original URL.
+    string decode(string shortUrl) {
+        return shortUrl;
+    }
+};
+
+int main () {
+    
+    return 0;
+}
